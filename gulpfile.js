9e39fa4dace0e4f7747287fbab762cc9302af05c
jaemin.han <jaemin.han@nyu.edu>
2018-04-15 16:13:43 -0400

    additional gulpfile and new page for topics in progress

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,19 @@ gulp.task('css-main', function(){
         .pipe(gulp.dest('./public/dist/css/'))
 })
 
+gulp.task('css-home', function(){
+    return gulp.src(
+            [
+                './public/plugins/datatables/dataTables.bootstrap4.min.css',
+                './public/plugins/datatables/responsive.bootstrap4.min.css'
+            ]
+        )
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(gp_concat('home.min.css'))
+        .pipe(gulp.dest('./public/dist/pages/home/css/'))
+})
+
 gulp.task('copy-fonts', function(){
     return gulp.src(
         ['./public/fonts/**']
@@ -29,7 +42,7 @@ gulp.task('copy-fonts', function(){
     .pipe(gulp.dest('./public/dist/fonts/'))
 })
 
-gulp.task('style', ['css-main', 'copy-fonts'], function(){})
+gulp.task('style', ['css-main', 'copy-fonts', 'css-home'], function(){})
 
 gulp.task('css-vendor', function(){
     return gulp.src(
@@ -108,26 +121,28 @@ gulp.task('js-page-dashboard', function(){
             ]
         )
         .pipe(gp_concat('dashboard.min.js'))
-        .pipe(gulp.dest('./public/dist/js/'))
+        .pipe(gulp.dest('./public/dist/pages/dashboard/js/'))
         .pipe(gp_rename('dashboard.min.js'))
         .pipe(gp_uglify())
         .pipe(gulp.dest('./public/dist/pages/dashboard/js/'))
 });
 
-// gulp.task('js-main', function(){
-//     return gulp.src(
-//             [
-//                 './public/js/main.js'
-//             ]
-//         )
-//         .pipe(gp_concat('main.min.js'))
-//         .pipe(gulp.dest('./public/dist/js/'))
-//         .pipe(gp_rename('main.min.js'))
-//         .pipe(gp_uglify())
-//         .pipe(gulp.dest('./public/dist/js/'))
-// });
+gulp.task('js-page-home', function(){
+    return gulp.src(
+            [   
+                './public/plugins/datatables/jquery.dataTables.min.js',
+                './public/plugins/datatables/dataTables.bootstrap4.min.js',
+                './public/plugins/datatables/dataTables.responsive.min.js'
+            ]
+        )
+        .pipe(gp_concat('home.min.js'))
+        .pipe(gulp.dest('./public/dist/pages/home/js/'))
+        .pipe(gp_rename('home.min.js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('./public/dist/pages/home/js/'))
+});
 
-gulp.task('js', ['js-vendor', 'js-main'], function(){})
+gulp.task('js', ['js-vendor', 'js-app', 'js-page-dashboard', 'js-page-home'], function(){})
 
 gulp.task('prod', ['style', 'js'], function(){})
 gulp.task('default', ['style', 'js'], function(){})
